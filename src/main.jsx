f4d9079f9cb05ce9960f95bd80eccd62367aadfd
rodrigo chaves <chaves26rodrigo@gmail.com>
2023-05-24 17:00:57 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import './components/pages/itemListContainer/ItemListContainer.css'
+import'./components/layout/navBar/Navbar.css'
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
