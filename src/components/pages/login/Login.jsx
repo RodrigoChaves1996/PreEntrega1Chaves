a3a289107a535fc5a0466ec6770860fb8f3a7e8b
rodrigo chaves <chaves26rodrigo@gmail.com>
2023-06-15 15:25:49 -0300

    Agregando Cambios

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,9 +1,59 @@
-import { TextField } from "@mui/material";
-
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField,} from "@mui/material";
+import { useState } from "react";
 
 export const Login = () => {
-  return  <div>
-    <TextField id="outlined-basic" label="Login" variant="outlined" />
-    </div>;
-  
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        justifyContent: "flex-stard",
+        flexDirection: "revert",
+        display: "flex",
+        alignItems: "center",
+        gap: "10px"
+      }}
+    >
+      <TextField
+        label="Email"
+        variant="outlined"
+        color="success"
+        size="medium"
+      />
+
+      <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+        <InputLabel color="success" htmlFor="outlined-adornment-password">
+          Contraseña
+        </InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          color="success"
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword(!showPassword)}
+                edge="end"
+              >
+                {showPassword ? (
+                  <VisibilityOff />
+                ) : (
+                  <Visibility color="success" />
+                )}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+
+      <Button variant="contained" color="success">
+        Ingrear
+      </Button>
+    </Box>
+  );
 };
+
