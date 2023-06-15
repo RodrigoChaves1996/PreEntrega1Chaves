c9ac28726771a0e824b38f168ee43495a17951c3
rodrigo chaves <chaves26rodrigo@gmail.com>
2023-06-15 13:04:43 -0300

    Agregando Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/layout/navBar/Navbar";
 import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
+import { Login } from "./components/pages/login/Login";
 
 const App = () => {
 
   return (<div>
       <Navbar />
+      <Login />
       <ItemListContainer greeting={'Los mejores precios en Accesorios y Productos'}/>
+
+
   </div>
   )
 }
---
+++ src/components/pages/login/Login.jsx
@@ -0,0 +1,9 @@
+import { TextField } from "@mui/material";
+
+
+export const Login = () => {
+  return  <div>
+    <TextField id="outlined-basic" label="Login" variant="outlined" />
+    </div>;
+  
+};
